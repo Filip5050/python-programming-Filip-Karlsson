0b41c6929916f3336545c3dc10a55310e2fb1d6c
Filip5050 <filipkarlsson5050@gmail.com.>
2024-09-16 18:15:34 +0200

    code

---
+++ Excercises/Excercises.py
@@ -0,0 +1,50 @@
+import math
+
+catheti = (4**2) + (3**2)
+hypothenuse = math.sqrt(catheti)
+hypothenuse
+
+print(f'The Hypotenuse has a length of {hypothenuse}')
+
+
+hypothenuse = (7**2) - (5**2)
+
+
+hypothenuse = math.sqrt(hypothenuse)
+catheti = round(hypothenuse, 1)
+print(f'the catheti has a length of around {catheti}')
+
+accuracy = 300/365
+
+dec_accuracy = accuracy * 100
+
+print(f"the accuracy of this model is {round(dec_accuracy,2)}%")
+
+
+
+
+import math
+
+Addition = (2+985)
+
+Addition2 = (2+2+11+985)
+
+Divide = (Addition/Addition2)
+print(f'The accuracy is {Divide}')
+
+x = (4-0)
+y = (4-1)
+k = y/x
+
+print(f'The slope is {k} + 1 which is 1.75')
+
+
+catheti = (5**2) + (1**2)
+hypothenuse = math.sqrt(catheti)
+hypothenuse = round(hypothenuse, 1)
+print(f"The distance between P1 and P2 is {hypothenuse}")
+
+distance = ((2-3)**2) + ((1-1)**2) + ((4-0)**2)
+distance = math.sqrt(distance)
+distance =round(distance, 1)
+print(f"Distans between the points is {distance} l.u")	
