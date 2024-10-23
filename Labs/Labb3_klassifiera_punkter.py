a7b21ffd6ea8c166a2549cb325721b80b53382fd
Filip5050 <filipkarlsson5050@gmail.com.>
2024-10-23 11:35:54 +0200

    Klassifierar punkterna, skrivit en README fil

--- Labs/Labb3_klassifiera_punkter.py
+++ Labs/Labb3_klassifiera_punkter.py
@@ -1,52 +1,44 @@
-
+import matplotlib.pyplot as plt
 import numpy as np
+
+
+
 X = []
 Y = []
+
 with open(r"C:\Code\python-programming-Filip-Karlsson\Data\unlabelled_data.csv", "r") as f_read:
-   lines = f_read.readlines()
-   for line in lines:
-      split = line.split()
-      x, y = map(float,line.split(",")) #sepererar punkterna/kordinaterna så jag kan få in rätt nummer i respektive lista
-      X.append(x) #lägger in x kordinaterna in i min X fil
-      Y.append(y) #lägger in y kordinaterna in i min y fil
+    lines = f_read.readlines()
+    for line in lines:
+        x, y = map(float, line.split(","))
+        X.append(x)
+        Y.append(y) 
 
-#Förberear jag min linje till grafen, där jag har valt en x och y linje som sepererar grafen ungefär i mitten
+
 x_line = [-5.53, 4.74]
-y_line = [-4.19,3.40]
+y_line = [-4.19, 3.40]
 
 
+slope = (y_line[1] - y_line[0]) / (x_line[1] - x_line[0])
+intercept = y_line[0] - slope * x_line[0]
 
-
-
-
-
-def point_position(x, y, x_line, y_line):
-    
-    #här gör vi två variabler och ger dom värdena från listans som läggs in som argument i funktionen.
-    x1, y1 = x_line
-    x2, y2 = y_line
-    #här räknar jag ut m värdet i linjära vinkeln ekvationen y=kx+m
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
-    y_on_line = m * x + b
-    # Determine position
-    if y > y_on_line:
-        return "1"
-    else:
-        return "0"
-    
-positions = []   
-# Check position of each point relative to the line
-for x, y in zip(X, Y):
-    position = point_position(x, y, x_line, y_line)
-    positions.append(position)
-
-
-with open(r"C:\Code\python-programming-Filip-Karlsson\Data\labelled_data.csv", "w") as f_write:
-    # Optionally write a header
-    f_write.write("x,y,position\n")
-    
+def classify_points(X, Y, slope, intercept):
+    labels = []
+    #for loopen går igenom alla punkter och ränkar ut y-värdet för linjen vid den x-koordinaten.
+    #Beroende på y värdet är över eller under den uträknade y värdet så läggs värdena in i above_line listan eller below_line listan
     for x, y in zip(X, Y):
-        position = point_position(x, y, x_line, y_line)
-        # Write the point and its classification to the new file
-        f_write.write(f"{x},{y},{position}\n")
+        y_on_line = slope * x + intercept
+        if y > y_on_line:
+            labels.append((x, y, 1))  # Sparar 1 ifall den är över linjen
+        else:
+            labels.append((x, y, 0))  # Spara 0 ifall den är under linjen
+    
+    return labels
+
+#kallar på funktionen
+labelled_points = classify_points(X, Y, slope, intercept)
+
+# Write labelled points to a new file
+with open(r"C:\Code\python-programming-Filip-Karlsson\Data\labelled_data.csv", "w") as f_write:
+    f_write.write("1 above the line, 0 under the line \n")
+    for x, y, label in labelled_points:
+        f_write.write(f"{x},{y},{label}\n")
--- Labs/labb3.py
+++ Labs/labb3.py
@@ -13,50 +13,44 @@ with open(r"C:\Code\python-programming-Filip-Karlsson\Data\unlabelled_data.csv",
       X.append(x) #lägger in x kordinaterna in i min X fil
       Y.append(y) #lägger in y kordinaterna in i min y fil
 
-
-
-#Förberear jag min linje till grafen, där jag har valt en x och y linje som sepererar grafen ungefär i mitten
+#Förbereder jag min linje till grafen, där jag har valt en x och y linje som sepererar grafen ungefär i mitten
 x_line = [-5.53, 4.74]
 y_line = [-4.19,3.40]
 
+#här beräknar koden lutningen = K på grafen och interceptorn
+slope = (y_line[1] - y_line[0]) / (x_line[1] - x_line[0])
+intercept = y_line[0] - slope * x_line[0]
 
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-def point_position(x, y, x_line, y_line):
+#Funktionen tar listan med x och y kordinater och linje parametern 
+def classify_points(X, Y, slope, intercept):
+    above_line = []
+    below_line = []
+    #for loopen går igenom alla punkter och ränkar ut y-värdet för linjen vid den x-koordinaten.
+    #Beroende på y värdet är över eller under den uträknade y värdet så läggs värdena in i above_line listan eller below_line listan
+    for x, y in zip(X, Y):
+        y_on_line = slope * x + intercept
+        if y > y_on_line:
+            above_line.append((x, y))
+        else:
+            below_line.append((x, y))
     
-    #här gör vi två variabler och ger dom värdena från listans som läggs in som argument i funktionen.
-    x1, y1 = x_line
-    x2, y2 = y_line
-    #här räknar jag ut m värdet i linjära vinkeln ekvationen y=kx+m
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
-    y_on_line = m * x + b
-    # Determine position
-    if y > y_on_line:
-        return "right"
-    else:
-        return "left"
-   
-   # Check position of each point relative to the line
-for x, y in zip(X, Y):
-    position = point_position(x, y, x_line, y_line)
-    print(f"Point ({x}, {y}) is {position} the line.")
+    return above_line, below_line
+
+#kallar på funktionen
+above_line, below_line = classify_points(X, Y, slope, intercept)
+
 
 
 plt.figure(figsize=(10, 6))
-plt.scatter(X,Y)
-plt.plot(x_line,y_line)
-plt.title('Scatter plot of the the coordinates in unlabelled_data')
+plt.plot(x_line, y_line, color='red', label='Line')
+if above_line:
+    plt.scatter(*zip(*above_line), color='black', label='Above Line', marker='o')
+if below_line:
+    plt.scatter(*zip(*below_line), color='yellow', label='Below Line', marker='o')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.grid()
-
-
-
-
+plt.legend()
 plt.show()
 
     
